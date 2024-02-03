9cc79305c66f061a637c95b34c235408e6e698b6
Ashley Nord <anord@hntb.com>
2024-02-03 03:26:52 -0500

    added the connection file for mysql2

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require ("mysql2");
+
+// Connect to database
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      password: 'rootroot',
+      database: 'employee_tracker_db'
+    });
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
